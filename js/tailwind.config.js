d113eefc4419f47db2e14fe5c04da3d8bab4497f
alvintran888 <106613854+alvintran888@users.noreply.github.com>
2024-09-09 18:31:16 +0700

    responsive not yet

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                manrope: ['Manrope', 'sans-serif']
+            },
+            backgroundImage: {
+                'custom-gradient': 'linear-gradient(to right, #02076F 70%, #2100B0 30%)'
+            }
+        }
+    }
+}
